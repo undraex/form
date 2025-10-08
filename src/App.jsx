ee38e3d0c3a8257cbf067c348b78a59b8663edce
undraex <undrahx@gmail.com>
2025-10-08 16:23:56 +0800

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [user, setUser] = useState("");
+
+  return;
+  <></>;
+}
+
+export default App;
