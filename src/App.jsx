11df9c82fda60389eb9a0ee5c63ee5919a047682
undraex <undrahx@gmail.com>
2025-10-11 18:21:58 +0800

    first gshquyu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
-import { useState } from "react";
+import { StrictMode, useState } from "react";
 import "./App.css";
+import { Step } from "./step1.jsx";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/step1" element={<step1 />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
 
 function App() {
   const [first, setFirst] = useState("");
   const [last, setLast] = useState("");
   const [user, setUser] = useState("");
 
-  return;
-  <></>;
+  return (
+    <div className="container">
+      <div className="card">
+        <img src="./logo.svg" className="logo"></img>
+        <div className="join inter">Join Us! 😎</div>
+        <p className="desc inter">
+          Please provide all current information accurately.
+        </p>
+        <div className="first-name inter">
+          <p className="first">First name</p>
+          <p className="star">*</p>
+        </div>
+        <div>
+          <input className="input"></input>
+        </div>
+        <div className="last-name inter">
+          <p className="last">Last name</p>
+          <p className="star">*</p>
+        </div>
+        <div>
+          <input className="input"></input>
+        </div>
+        <div className="username inter">
+          <p className="user">Username</p>
+          <p className="star">*</p>
+        </div>
+        <div>
+          <input className="input"></input>
+        </div>
+        <div>
+          <button className="continue1 inter">Continue 1/3 </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/step1.jsx
@@ -0,0 +1,3 @@
+export function Step1() {
+  return <div>Step</div>;
+}
